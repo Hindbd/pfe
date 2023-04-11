486159d6dc21dbdc8dd03d4e0e2d90a11dc58fb0
Hindbd <hindbouhedda0@gmail.com>
2023-04-11 18:20:31 +0000

    .

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,19 +14,6 @@
                             <label for="prenom" class="col-md-4 col-form-label text-md-end">{{ __('Prenom') }}</label>
 
                             <div class="col-md-6">
-                                <!-- <input id="prenom" type="text" class="form-control @error('prenom') is-invalid @enderror" name="prenom" value="{{ old('prenom') }}" required autocomplete="prenom" autofocus> -->
-                                <input type="text" name="prenom">
-                                @error('prenom')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-                        <div class="row mb-3">
-                            <label for="prenom" class="col-md-4 col-form-label text-md-end">{{ __('Prenom') }}</label>
-
-                            <div class="col-md-6">
                                 <input id="prenom" type="text" class="form-control @error('prenom') is-invalid @enderror" name="prenom" value="{{ old('prenom') }}" required autocomplete="prenom" autofocus>
 
                                 @error('prenom')
@@ -81,23 +68,7 @@
                                 @enderror
                             </div>
                         </div>
-                        <div class="row mb-3">
-                            <label for="statut" class="col-md-4 col-form-label text-md-end">{{ __('Statut') }}</label>
 
-                            <div class="col-md-6">
-                            <select id="role" name="role">
-                                <option value="Prof">Prof</option>
-                                <option value="Doctorant">Doctorant</option>
-                                <option value="Partenaire">Partenaire</option>
-                                <option value="Autre">Autre</option>
-                            </select>
-                                @error('statut')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
                         <div id="doctorant_only" style="display:none;"  >
                             <div class="row mb-3">
                                 <label for="statut" class="col-md-4 col-form-label text-md-end">{{ __('Encadrant') }}</label>
