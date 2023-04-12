b648b2e20e0a09c746c79052355439508f9d0579
Hindbd <hindbouhedda0@gmail.com>
2023-04-12 23:51:26 +0000

    nn

--- database/migrations/2023_04_12_231644_create_projects_table.php
+++ database/migrations/2023_04_12_231644_create_projects_table.php
@@ -26,7 +26,7 @@ return new class extends Migration
             $table->foreign('axes')
             ->references('id')
             ->on('axes')
-            ->
+            
         });
     }
 
